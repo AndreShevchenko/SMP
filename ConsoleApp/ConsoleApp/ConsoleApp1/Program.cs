39cf0f4a02256dc32e247d71df2b483d8ce066d1
LAPTOP-23RP8AMA\hellk <hellkar8@gmail.com>
2018-10-21 12:01:08 +0300

    no message

---
+++ ConsoleApp/ConsoleApp/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину прямоугольника");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите ширину прямоугольника");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Вот Ваш прямоугольник:");
+            Print.PrintRectangle(a, b);
+            Console.ReadLine();
+        }
+    }
+}
+
+class Print
+{
+    public static void PrintLine(int a)
+    {
+        for (int i=1; i<=a; i++)
+        {
+            Console.Write("*");
+        }
+    }
+
+    public static void PrintRectangle (int a, int b)
+    {
+        for (int j=1; j<=b; j++)
+        {
+            PrintLine(a);
+            Console.Write("\n");
+        }
+    }
+}
