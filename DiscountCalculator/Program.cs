7b0ce600d8fc93ba4143692a1496b7948993c37d
LAPTOP-23RP8AMA\hellk <hellkar8@gmail.com>
2018-10-22 15:44:52 +0300

    no message

---
+++ DiscountCalculator/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+
+namespace DiscountСalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вы хотите добавить новый продукт? 1 - да, 2 - нет");
+
+            int.TryParse(Console.ReadLine(), out var answer);
+
+            if (answer != 1)
+                return;
+
+            CreateProduct();
+
+            Console.ReadLine();
+        }
+
+        private static void CreateProduct()
+        {
+            var product = new Product();
+
+            Console.WriteLine("Введите название продукта");
+
+            product.Name = Console.ReadLine();
+
+            Console.WriteLine("Введите стоимость продукта");
+
+            int.TryParse(Console.ReadLine(), out var price);
+
+            while (price <= 0)
+            {
+                Console.WriteLine("Стоимость продукта не была введена или она была введена с ошибкой. Пожалуйста, введите стоимость продукта ещё раз");
+
+                int.TryParse(Console.ReadLine(), out price);
+            }
+
+            product.Price = price;
+
+            Console.WriteLine("Выберите тип скидки 1 - процент от стоимости, 2 - сумма от стоимости");
+
+            int.TryParse(Console.ReadLine(), out var answer2);
+
+            while (answer2 != 1 & answer2 != 2)
+            {
+                Console.WriteLine("Некорректный ответ");
+
+                Console.WriteLine("Выберите тип скидки 1 - процент от стоимости, 2 - сумма от стоимости");
+
+                int.TryParse(Console.ReadLine(), out answer2);
+            }
+
+            if (answer2 == 1)
+            {
+                Console.WriteLine("Введите значение скидки на товар (в % от общей стоимости)");
+
+                int.TryParse(Console.ReadLine(), out var percentDiscountValue);
+
+                while (percentDiscountValue > 100 || percentDiscountValue < 0)
+                {
+                    Console.WriteLine("Значение скидки не было введено или оно было введено с ошибкой. Пожалуйста, введите значение скидки (в % от общей стоимости) ещё раз");
+
+                    int.TryParse(Console.ReadLine(), out percentDiscountValue);
+                }
+
+                int discountValue = product.Price * percentDiscountValue / 100;
+
+                product.DiscountValue = discountValue;
+            }
+
+            if (answer2 == 2)
+            {
+                Console.WriteLine("Введите значение скидки на товар");
+
+                int.TryParse(Console.ReadLine(), out var discountValue);
+
+                while (discountValue < 0)
+                {
+                    Console.WriteLine("Значение скидки не было введено или оно было введено с ошибкой. Пожалуйста, введите значение скидки ещё раз");
+
+                    int.TryParse(Console.ReadLine(), out discountValue);
+                }
+
+                product.DiscountValue = discountValue;
+            }
+
+            Console.WriteLine("У скидки есть срок действия? 1 - да, 2 - нет");
+
+            int.TryParse(Console.ReadLine(), out var answer3);
+
+            while (answer3 != 1 & answer3 != 2)
+            {
+                Console.WriteLine("Некорректный ответ");
+
+                Console.WriteLine("У скидки есть срок действия? 1 - да, 2 - нет");
+
+                int.TryParse(Console.ReadLine(), out answer3);
+            }
+
+            if (answer3 == 1)
+            {
+                Console.WriteLine("Введите дату начала действия скидки (dd.mm.yyyy)");
+
+                DateTime.TryParse(Console.ReadLine(), out var startSellDate);
+                
+                if (startSellDate != DateTime.MinValue)
+                {
+                    product.StartSellDate = startSellDate;
+                }
+
+                Console.WriteLine("Введите дату окончания действия скидки (dd.mm.yyyy)");
+
+                DateTime.TryParse(Console.ReadLine(), out var endSellDate);
+
+                if (endSellDate != DateTime.MinValue)
+                {
+                    product.EndSellDate = endSellDate;
+                }
+            }
+
+            if (answer3 == 2)
+            {
+                product.StartSellDate = DateTime.UtcNow.AddDays(-1); //устанавливает вчера как дату начала действия скидки
+
+                product.EndSellDate = DateTime.UtcNow.AddDays(1);    //устанавливает завтра как дату окончания действия скидки               
+            }
+            
+            Console.WriteLine($"Вы успешно добавили новый продукт: {product.Name}, стоимость - {product.Price}р. {product.GetSellInformation()}");
+        }
+    }
+}
