424e8b8a1617e61317e450f2eb7170d0eaa6471e
LAPTOP-23RP8AMA\hellk <hellkar8@gmail.com>
2018-11-03 23:35:06 +0300

    no message

---
+++ Shop 2.0/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Data;
+
+namespace MobileStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        ShopContext db;
+        public HomeController(ShopContext context)
+        {
+            db = context;
+        }
+        public IActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+    }
+}
---
+++ Shop 2.0/Data/SampleData.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using Shop.Models;
+
+namespace Shop.Data
+{
+    public class SampleData
+    {
+        public static void Initialize(ShopContext context)
+        {
+            if (!context.Products.Any())
+            {
+                context.Products.AddRange(
+                    new Product
+                    {
+                        Name = "Хлеб",
+                        Price = 50
+                    },
+                    new Product
+                    {
+                        Name = "Колбаса",
+                        Price = 300
+                    },
+                    new Product
+                    {
+                        Name = "Сыр",
+                        Price = 500
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
