89543bcaabaefd3d55ee4f6495f56013c7d82aaa
LAPTOP-23RP8AMA\hellk <hellkar8@gmail.com>
2018-11-14 16:02:49 +0300

    no message

--- Shop/Controllers/CartsController.cs
+++ Shop/Controllers/CartsController.cs
@@ -10,32 +10,33 @@ using System.Threading.Tasks;
 
 namespace Shop.Controllers
 {
-    [Route("orders")]
-    public class OrdersController : Controller
+    [Route("cart")]
+    public class CartsController : Controller
     {
         public ShopContext _db;
 
-        public OrdersController(ShopContext db)
+        public CartsController(ShopContext db)
         {
             _db = db;
         }
 
-        [Route("")]        
+        [Route("")]
         public async Task<IActionResult> Index()
         {
             var orders = await _db.Orders
+                .Where(x => x.Status == OrderStatus.NotPayed)
                 .Include(x => x.Items)
                 .ThenInclude(x => x.Product)
                 .ToListAsync();
 
-            var ordersViewModel = orders
-                .Select(order => new OrderViewModel
+            var cartsViewModel = orders
+                .Select(order => new CartsViewModel
                 {
                     Id = order.Id,
                     Number = order.Number,
                     Status = order.Status,
                     Items = order.Items
-                        .Select(item => new OrderItemViewModel
+                        .Select(item => new CartItemViewModel
                         {
                             ProductName = item.Product.Name,
                             ProductPrice = item.Product.Price,
@@ -43,11 +44,11 @@ namespace Shop.Controllers
                         }).ToList()
                 });
 
-            return View(ordersViewModel);
+            return View(cartsViewModel);
         }
 
         [HttpGet]
-        [Route("/add{productId:int}/payed")]        
+        [Route("/add{productId:int}/payed")]
         public async Task<ActionResult<bool>> Payed(int orderId)
         {
             var order = await _db.Orders.FirstOrDefaultAsync(x => x.Id == orderId);
---
+++ Shop/ViewModels/CartsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.ViewModels
+{
+    public class CartsViewModel
+    {
+        public int Id { get; set; }
+        public string Number { get; set; }
+        public decimal TotalSum => Items.Sum(x => x.TotalSum);
+        public List<CartItemViewModel> Items { get; set; }
+        public OrderStatus Status { get; internal set; }
+    }
+
+    public class CartItemViewModel
+    {
+        public decimal TotalSum => ProductPrice * Count;
+
+        public string ProductName { get; set; }
+        public int Count { get; set; }
+        public decimal ProductPrice { get; set; }
+    }
+}
